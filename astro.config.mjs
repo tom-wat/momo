164af2a787a50d3a1c8d112c0dcb1355289a99a6
tom-wat <52376398+tom-wat@users.noreply.github.com>
2024-09-10 17:38:09 +0900

    設定の変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://momo-f1bcz6tij-wats-projects-297db0f9.vercel.app/",
+  site: "https://momo-sage.vercel.app/",
   base: "/",
 });
